9b14b0d493fda34683785422be3504591fdbc560
Faton Hoti <hotifaton@outlook.com>
2024-09-21 23:35:50 +0200

    Add command-line arguments

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 	"time"
@@ -16,12 +17,32 @@ type Task struct {
 }
 
 type TaskManager struct {
-	tasks []Task
-	path  string
+	currMaxId int32
+	tasks     []Task
+	path      string
 }
 
-func (tm *TaskManager) AddTask(task Task) {
-	tm.tasks = append(tm.tasks, task)
+func (tm *TaskManager) Init() {
+	tm.FromFile()
+	var maxId int32 = 0
+	for _, v := range tm.tasks {
+		if v.ID > maxId {
+			maxId = v.ID
+		}
+	}
+	tm.currMaxId = maxId
+}
+
+func (tm *TaskManager) AddTask(desc string) {
+	newTask := Task{
+		tm.currMaxId + 1,
+		desc,
+		false,
+		time.Now(),
+		time.Time{},
+	}
+	tm.tasks = append(tm.tasks, newTask)
+	tm.currMaxId++
 }
 
 func (tm *TaskManager) DeleteTask(id int32) {
@@ -33,16 +54,25 @@ func (tm *TaskManager) DeleteTask(id int32) {
 		}
 	}
 	tm.tasks = append(tm.tasks[:idx], tm.tasks[idx+1:]...)
+	// TODO: Shift IDs
 }
 
 func (tm *TaskManager) ListTasks() {
-	for _, task := range tm.tasks {
-		fmt.Printf("ID=%d\nDescription=%s\nCompleted=%t\nCreatedAt=%s\nCompletedAt=<TODO>\n----\n", task.ID, task.Description, task.Completed, task.CompletedAt)
+	for i := range tm.tasks {
+		task := &tm.tasks[i]
+		fmt.Printf("ID=%d\nDescription=%s\nCompleted=%t\nCreatedAt=%s\n", task.ID, task.Description, task.Completed, task.CreatedAt)
+		if task.Completed {
+			fmt.Printf("%s\n", task.CompletedAt)
+		} else {
+			fmt.Printf("CompletedAt=NOT_COMPLETED\n")
+			fmt.Printf("----\n")
+		}
 	}
 }
 
 func (tm *TaskManager) CompleteTask(id int32) {
-	for _, task := range tm.tasks {
+	for i := range tm.tasks {
+		task := &tm.tasks[i]
 		if task.ID == id {
 			task.Completed = true
 			task.CompletedAt = time.Now()
@@ -98,44 +128,98 @@ func (tm *TaskManager) FromFile() {
 
 func main() {
 
+	var desc = flag.String("add", "", "Add a new task with a description.")
+	var delete = flag.Int("delete", -271828, "Remove a specific task from the list.")
+	var complete = flag.Int("complete", -271828, "Mark a specific task as completed.")
+	var list = flag.Bool("list", false, "Display all tasks.")
+	var help = flag.Bool("help", false, "Displays usage instructions.")
+
+	flag.Parse()
+
+	if *help {
+		fmt.Println("Usage: go run main.go [flag]")
+		fmt.Println("Options:")
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	usedFlags := 0
+
+	if *desc != "" {
+		usedFlags++
+	}
+
+	if *delete != -271828 {
+		usedFlags++
+	}
+
+	if *complete != -271828 {
+		usedFlags++
+	}
+
+	if *list {
+		usedFlags++
+	}
+
+	if usedFlags > 1 {
+		fmt.Println("Error: Only one flag can be used at a time. Use '-help' to see usage instructions.")
+		os.Exit(1)
+	}
+
 	taskManager := TaskManager{
+		1,
 		make([]Task, 0),
 		"./tasks.json",
 	}
+	taskManager.Init()
 
-	// Load tasks from disc
-	fmt.Println("Loading tasks from disc...")
-	taskManager.FromFile()
-
-	t1 := Task{
-		1,
-		"Some Description",
-		false,
-		time.Now(),
-		time.Now(),
+	// Handle each flag case
+	if *desc != "" {
+		taskManager.AddTask(*desc)
+	} else if *delete != -271828 {
+		taskManager.DeleteTask(int32(*delete))
+	} else if *complete != -271828 {
+		taskManager.CompleteTask(int32(*complete))
+	} else if *list {
+		taskManager.ListTasks()
+	} else {
+		fmt.Println("No valid flag provided. Use -help for usage information.")
+		os.Exit(1)
 	}
 
-	t2 := Task{
-		2,
-		"Some Other Description",
-		true,
-		time.Now(),
-		time.Now(),
-	}
-
-	taskManager.AddTask(t1)
-	taskManager.AddTask(t2)
-
-	fmt.Println("Tasks:")
-	taskManager.ListTasks()
-
-	fmt.Println("Saving tasks to file...")
 	taskManager.ToFile()
 
-	fmt.Println("Loading tasks from file...")
-	taskManager.FromFile()
+	/*
+		t1 := Task{
+			1,
+			"Some Description",
+			false,
+			time.Now(),
+			time.Now(),
+		}
 
-	fmt.Println("Tasks after loading:")
-	taskManager.ListTasks()
+		t2 := Task{
+			2,
+			"Some Other Description",
+			true,
+			time.Now(),
+			time.Now(),
+		}
+
+		taskManager.AddTask(t1)
+		taskManager.AddTask(t2)
+
+		fmt.Println("Tasks:")
+		taskManager.ListTasks()
+
+		fmt.Println("Saving tasks to file...")
+		taskManager.ToFile()
+
+		fmt.Println("Loading tasks from file...")
+		taskManager.FromFile()
+
+		fmt.Println("Tasks after loading:")
+		taskManager.ListTasks()
+	*/
 
 }
